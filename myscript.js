addb183d8eba3b934222bf0fe8df744307d3a4c3
Manuel Guerrini <man.guerrini@gmail.com>
2019-11-06 16:46:22 +0100

    Rendo verdi i quadrati al click

--- myscript.js
+++ myscript.js
@@ -2,3 +2,9 @@
 // 15 di questi quadratini (scelti a caso all’inizio) se cliccati diventano rossi,
 // gli altri diventano verdi (tutti i rimanennti)
 // Opzionale: Sopra alla griglia deve esserci un contatore che conta quanti rossi e quanti verdi sono stati scoperti
+
+// $(".quadrato").css("background", "green");
+
+$(".quadrato").click(function(){
+  $(this).css("background", "green");
+});
